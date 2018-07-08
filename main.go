eba962ade0bebe8e87f6483439e8521862fe8c88
ebinks <elizabethjbinks@gmail.com>
2018-07-08 15:26:37 -0400

    implemented GenKeyImage function

--- main.go
+++ main.go
@@ -2,33 +2,36 @@ package main
 
 import (
 	"fmt"
-	"github.com/btcsuite/btcec"
-	"github.com/btcsuite/chaincfg/chainhash"
+	"log"
+	"crypto/sha256"
+	//"github.com/btcsuite/btcec"
 	"github.com/noot/ring-go/ring"
-//	"encoding/hex"
+	//"encoding/hex"
 )
 
 func main() {
 	fmt.Println("starting ring-go...")
 	fmt.Println("starting generation of keys...")
 
-	ring.Gen();
+	/* generate new private public keypair */
+	//privkey, err := ring.GenPrivkey()
+	privkey, pubkey := ring.GenKeysFromStr("358be44145ad16a1add8622786bef07e0b00391e072855a5667eb3c78b9d3803")
+	/*if err != nil {
+		log.Fatal(err)
+	}*/
+	//pubkey := privkey.PubKey()
 
-	privkey, err := btcec.NewPrivateKey(btcec.S256());
-//	pkBytes, err := hex.DecodeString("22a47fa09a223f2aa079edf85a7c2d4f8720ee63e502ee2869afab7de234b80c")
-//	pkBytes, err := hex.DecodeString("358be44145ad16a1add8622786bef07e0b00391e072855a5667eb3c78b9d3803")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-//	privkey, pubkey := btcec.PrivKeyFromBytes(btcec.S256(), pkBytes)
-	pubkey := privkey.PubKey()
+	/* generate public key image */
+	image := ring.GenKeyImage(privkey)
+	fmt.Println(image)
+
+	/* sign message */
 	msg := "helloworld"
-	msgHash := chainhash.DoubleHashB([]byte(msg))
+	msgHashArr := sha256.Sum256([]byte(msg))
+	msgHash := msgHashArr[:]
 	sig, err := privkey.Sign(msgHash)
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
 
 	verified := sig.Verify(msgHash, pubkey)
--- ring/ring.go
+++ ring/ring.go
@@ -1,6 +1,59 @@
 package ring
 
+import (
+	"fmt"
+	"github.com/btcsuite/btcec"
+	"encoding/hex"
+	"crypto/sha256"
+	"math/big"
+//	"strings"
+)
+
+func GenKeysFromStr(str string) (*btcec.PrivateKey, *btcec.PublicKey) {
+	pkBytes, err := hex.DecodeString(str)
+	if err != nil  { return nil, nil }
+	privkey, pubkey := btcec.PrivKeyFromBytes(btcec.S256(), pkBytes)
+	return privkey, pubkey
+}
+
+func GenPrivkey() (*btcec.PrivateKey, error) {
+        privkey, err := btcec.NewPrivateKey(btcec.S256());
+        if err != nil {
+                fmt.Println(err)
+                return nil, err
+        }
+	return privkey, err
+}
+
+func GenPubkey(privkey *btcec.PrivateKey) (*btcec.PublicKey) {
+	pubkey := privkey.PubKey()
+	return pubkey
+}
+
+func GenKeyImage(privkey *btcec.PrivateKey) (*btcec.PublicKey) {
+	// get pubkey of privkey
+	pubkey := privkey.PubKey()
+
+	// create new pubkey object image
+	image := privkey.PubKey()
+
+	// hash pubkey.X
+        hashX := sha256.Sum256(pubkey.X.Bytes())
+        image.X = new(big.Int).SetBytes( hashX[:] )
+	// set image.X to hash of pubkey.X * privkey.D
+	image.X.Mul(image.X, privkey.D)
+
+	// hash pubkey.Y
+	hashY := sha256.Sum256(pubkey.Y.Bytes())
+	image.Y = new(big.Int).SetBytes( hashY[:] )
+	// set image.Y to hash of pubkey.Y * privkey.D
+	image.Y.Mul(image.Y, privkey.D)
+
+	return image
+}
+
 func Sign() { }
-func Gen() { }
+
 func Ver() { }
+
 func Link() { }
